46de934dfdc3e679acb1980baac698779fffbf29
sivanargana <siva.nargana66@gmail.com>
2024-12-05 16:50:57 +0530

    dot

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -14,8 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    document.cookie = "token101=123456; expires=;domain=sivanargana.github.io; path=/";
-    document.cookie = "token101=123456; expires=;domain=sravan3210.github.io; path=/";
+    document.cookie = "token101=123456; expires=;domain=.github.io; path=/";
 
    
   }
