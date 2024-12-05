9c0af9c4489e5a10339b72737e7ea8efb44b0c82
sivanargana <siva.nargana66@gmail.com>
2024-12-05 16:16:55 +0530

    new changes

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core'; 
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+ 
+ 
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+ 
+
+  ngOnInit(): void {
+
+    document.cookie = "token101=123456; expires=;domain=sivanargana.github.io path=/";
+
+   
+  }
+ 
 }
